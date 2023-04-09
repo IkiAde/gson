b2ed8fa2f5a543411a0fc065d31e6776e9215b87
ikimath <ikimath.adeoye.etu@univ-lille.fr>
2023-04-09 14:18:34 +0200

    retrait des methodes fail()

--- gson/src/test/java/com/google/gson/stream/JsonWriterTest.java
+++ gson/src/test/java/com/google/gson/stream/JsonWriterTest.java
@@ -71,88 +71,6 @@ public class JsonWriterTest extends TestCase{
   }
 
 
-  public void testInvalidTopLevelTypes() throws IOException {
-    jsonWriter.name("hello");
-    try {
-      jsonWriter.value("world");
-      fail();
-    } catch (IllegalStateException expected) {
-    }
-  }
-
-
-  public void testTwoNames() throws IOException {
-    jsonWriter.beginObject();
-    jsonWriter.name("a");
-    try {
-      jsonWriter.name("a");
-      fail();
-    } catch (IllegalStateException expected) {
-    }
-  }
-
-
-  public void testNameWithoutValue() throws IOException {
-    jsonWriter.beginObject();
-    jsonWriter.name("a");
-    try {
-      jsonWriter.endObject();
-      fail();
-    } catch (IllegalStateException expected) {
-    }
-  }
-
-
-  public void testValueWithoutName() throws IOException {
-    jsonWriter.beginObject();
-    try {
-      jsonWriter.value(true);
-      fail();
-    } catch (IllegalStateException expected) {
-    }
-  }
-
-
-  public void testMultipleTopLevelValues() throws IOException {
-    jsonWriter.beginArray().endArray();
-    try {
-      jsonWriter.beginArray();
-      fail();
-    } catch (IllegalStateException expected) {
-    }
-  }
-
-
-  public void testBadNestingObject() throws IOException {
-    jsonWriter.beginArray();
-    jsonWriter.beginObject();
-    try {
-      jsonWriter.endArray();
-      fail();
-    } catch (IllegalStateException expected) {
-    }
-  }
-
-
-  public void testBadNestingArray() throws IOException {
-    jsonWriter.beginArray();
-    jsonWriter.beginArray();
-    try {
-      jsonWriter.endObject();
-      fail();
-    } catch (IllegalStateException expected) {
-    }
-  }
-
-
-  public void testNullName() throws IOException {
-    jsonWriter.beginObject();
-    try {
-      jsonWriter.name(null);
-      fail();
-    } catch (NullPointerException expected) {
-    }
-  }
 
 
   public void testNullStringValue() throws IOException {
@@ -175,79 +93,10 @@ public class JsonWriterTest extends TestCase{
   }
 
 
-  public void testNonFiniteFloats() throws IOException {
-    jsonWriter.beginArray();
-    try {
-      jsonWriter.value(Float.NaN);
-      fail();
-    } catch (IllegalArgumentException expected) {
-      assertThat(expected).hasMessageThat().isEqualTo("Numeric values must be finite, but was NaN");
-    }
-    try {
-      jsonWriter.value(Float.NEGATIVE_INFINITY);
-      fail();
-    } catch (IllegalArgumentException expected) {
-      assertThat(expected).hasMessageThat().isEqualTo("Numeric values must be finite, but was -Infinity");
-    }
-    try {
-      jsonWriter.value(Float.POSITIVE_INFINITY);
-      fail();
-    } catch (IllegalArgumentException expected) {
-      assertThat(expected).hasMessageThat().isEqualTo("Numeric values must be finite, but was Infinity");
-    }
-  }
 
 
-  public void testNonFiniteDoubles() throws IOException {
-    jsonWriter.beginArray();
-    try {
-      jsonWriter.value(Double.NaN);
-      fail();
-    } catch (IllegalArgumentException expected) {
-      assertThat(expected).hasMessageThat().isEqualTo("Numeric values must be finite, but was NaN");
-    }
-    try {
-      jsonWriter.value(Double.NEGATIVE_INFINITY);
-      fail();
-    } catch (IllegalArgumentException expected) {
-      assertThat(expected).hasMessageThat().isEqualTo("Numeric values must be finite, but was -Infinity");
-    }
-    try {
-      jsonWriter.value(Double.POSITIVE_INFINITY);
-      fail();
-    } catch (IllegalArgumentException expected) {
-      assertThat(expected).hasMessageThat().isEqualTo("Numeric values must be finite, but was Infinity");
-    }
-  }
 
 
-  public void testNonFiniteNumbers() throws IOException {
-    jsonWriter.beginArray();
-    try {
-      jsonWriter.value(Double.valueOf(Double.NaN));
-      fail();
-    } catch (IllegalArgumentException expected) {
-      assertThat(expected).hasMessageThat().isEqualTo("Numeric values must be finite, but was NaN");
-    }
-    try {
-      jsonWriter.value(Double.valueOf(Double.NEGATIVE_INFINITY));
-      fail();
-    } catch (IllegalArgumentException expected) {
-      assertThat(expected).hasMessageThat().isEqualTo("Numeric values must be finite, but was -Infinity");
-    }
-    try {
-      jsonWriter.value(Double.valueOf(Double.POSITIVE_INFINITY));
-      fail();
-    } catch (IllegalArgumentException expected) {
-      assertThat(expected).hasMessageThat().isEqualTo("Numeric values must be finite, but was Infinity");
-    }
-    try {
-      jsonWriter.value(new LazilyParsedNumber("Infinity"));
-      fail();
-    } catch (IllegalArgumentException expected) {
-      assertThat(expected).hasMessageThat().isEqualTo("Numeric values must be finite, but was Infinity");
-    }
-  }
 
 
   public void testNonFiniteFloatsWhenLenient() throws IOException {
@@ -404,44 +253,8 @@ public class JsonWriterTest extends TestCase{
   }
 
 
-  public void testMalformedNumbers() throws IOException {
-    String[] malformedNumbers = {
-        "some text",
-        "",
-        ".",
-        "00",
-        "01",
-        "-00",
-        "-",
-        "--1",
-        "+1", // plus sign is not allowed for integer part
-        "+",
-        "1,0",
-        "1,000",
-        "0.", // decimal digit is required
-        ".1", // integer part is required
-        "e1",
-        ".e1",
-        ".1e1",
-        "1e-",
-        "1e+",
-        "1e--1",
-        "1e+-1",
-        "1e1e1",
-        "1+e1",
-        "1e1.0",
-    };
 
-    for (String malformedNumber : malformedNumbers) {
 
-      try {
-        jsonWriter.value(new LazilyParsedNumber(malformedNumber));
-        fail("Should have failed writing malformed number: " + malformedNumber);
-      } catch (IllegalArgumentException e) {
-        assertThat(e.getMessage()).isEqualTo("String created by class com.google.gson.internal.LazilyParsedNumber is not a valid JSON number: " + malformedNumber);
-      }
-    }
-  }
 
 
   public void testBooleans() throws IOException {
